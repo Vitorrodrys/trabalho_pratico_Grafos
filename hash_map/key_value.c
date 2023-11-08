2b078234e90300efac68750eb108d1f95c949081
Vitorrodrys <Vitorrodrys@gmail.com>
2023-11-08 20:15:43 -0300

    following of pattern of placing the begging of the name of the function the summary name of the TAD in that it is implement

--- hash_map/key_value.c
+++ hash_map/key_value.c
@@ -14,27 +14,27 @@ typedef struct KeyValue{
 
 KeyValue *create_key_value(char *key, void *value){
 
-    KeyValue *new = memory_alloc(NULL, sizeof(KeyValue));
+    KeyValue *new = me_memory_alloc(NULL, sizeof(KeyValue));
     new->key = key;
     new->value = value;
     return new;
 }
 
-const char *get_key(KeyValue *self){
+const char *kv_get_key(KeyValue *self){
     return self->key;
 }
-void *get_value(KeyValue *self){
+void *kv_get_value(KeyValue *self){
     return self->value;
 }
-void set_key(KeyValue *self, void *value){
+void kv_set_key(KeyValue *self, void *value){
     self->value = value;
 }
-int is_this_element_kv(KeyValue *self, KeyValue *other){
+int kv_is_this_element(KeyValue *self, KeyValue *other){
     return !strcmp(self->key, other->key);
 }
 void *destroy_kv(KeyValue *self){
 
-    free_memory((void *)&self);
+    me_free_memory((void *)&self);
     return NULL;
 }
 
--- hash_map/key_value.h
+++ hash_map/key_value.h
@@ -5,10 +5,10 @@
 #ifndef UNTITLED70_KEY_VALUE_H
 #define UNTITLED70_KEY_VALUE_H
 typedef struct KeyValue KeyValue;
-void set_key(KeyValue *self, void *value);
-void *get_value(KeyValue *self);
-const char *get_key(KeyValue *self);
+void kv_set_key(KeyValue *self, void *value);
+void *kv_get_value(KeyValue *self);
+const char *kv_get_key(KeyValue *self);
 KeyValue *create_key_value(char *key, void *value);
-int is_this_element_kv(KeyValue *self, KeyValue *other);
+int kv_is_this_element(KeyValue *self, KeyValue *other);
 void *destroy_kv(KeyValue *self);
 #endif //UNTITLED70_KEY_VALUE_H
