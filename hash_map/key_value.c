a6f6ef1b7971157c83762c60ace0f3877e2d64e7
Vitorrodrys <Vitorrodrys@gmail.com>
2023-11-10 10:01:25 -0300

    adding a pointer function to store a destructor of the value, by can destructor the value after

--- hash_map/key_value.c
+++ hash_map/key_value.c
@@ -5,18 +5,15 @@
 #include "key_value.h"
 #include "../memory/memory.h"
 #include <string.h>
+#include <stdio.h>
 
-typedef struct KeyValue{
-
-    void *value;
-    char *key;
-}KeyValue;
-
-KeyValue *create_key_value(char *key, void *value){
+KeyValue *create_key_value(const char *key, void *value, char* (*value_str)(void *), void *(*destructor)(void *)){
 
     KeyValue *new = me_memory_alloc(NULL, sizeof(KeyValue));
     new->key = strdup(key);
     new->value = value;
+    new->value_str = value_str;
+    new->destructor_value = destructor;
     return new;
 }
 
@@ -34,8 +31,17 @@ int kv_is_this_element(KeyValue *self, KeyValue *other){
 }
 void *destroy_kv(KeyValue *self){
 
+    me_free_memory((void *)&self->key);
+    if (self->destructor_value) {
+        self->value = self->destructor_value(self->value);
+    }
+    self->value_str = NULL;
     me_free_memory((void *)&self);
     return NULL;
 }
+char* kv_str(KeyValue *self){
 
+    char *string_v=self->value_str(self->value);
+    return me_formatted_str("\"%s\":%s", self->key, string_v);
+}
 
--- hash_map/key_value.h
+++ hash_map/key_value.h
@@ -4,11 +4,17 @@
 
 #ifndef UNTITLED70_KEY_VALUE_H
 #define UNTITLED70_KEY_VALUE_H
-typedef struct KeyValue KeyValue;
+typedef struct KeyValue{
+    void *value;
+    char *key;
+    char* (*value_str)(void *);
+    void *(*destructor_value)(void *);
+}KeyValue;
 void kv_set_key(KeyValue *self, void *value);
 void *kv_get_value(KeyValue *self);
 const char *kv_get_key(KeyValue *self);
-KeyValue *create_key_value(char *key, void *value);
+KeyValue *create_key_value(const char *key, void *value, char* (*value_str)(void *), void *(*destructor)(void *));
 int kv_is_this_element(KeyValue *self, KeyValue *other);
 void *destroy_kv(KeyValue *self);
+char* kv_str(KeyValue *self);
 #endif //UNTITLED70_KEY_VALUE_H
