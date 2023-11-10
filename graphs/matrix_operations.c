32ee7c7a2b0573fe53646beda62c2ff9d7a53883
Vitorrodrys <Vitorrodrys@gmail.com>
2023-11-10 09:59:27 -0300

    implements tranforming matrix in a string

--- graphs/matrix_operations.c
+++ graphs/matrix_operations.c
@@ -4,6 +4,7 @@
 
 #include "matrix_operations.h"
 #include "../memory/memory.h"
+#include <stdio.h>
 
 void *mop_create_matrix(size_t tam_type, int tam){
 
@@ -23,3 +24,35 @@ void *mop_destroy_matrix(void **matrix, int tam){
     me_free_memory((void *)&matrix);
     return NULL;
 }
+
+char *mop_str_matrix_int(double **matrix, int tam_matrix){
+
+    char *str_matrix = strdup("[\n");
+    char *aux;
+    char *aux2;
+    for (int i = 0; i < tam_matrix; ++i) {
+
+        aux = me_formatted_str("\t[ %4.2f,  ", matrix[i][0]);
+        aux2 = str_matrix;
+        str_matrix= me_concat_str(str_matrix, aux);
+        me_free_memory((void*)&aux2);
+        me_free_memory((void *)&aux);
+        for (int j = 1; j < tam_matrix-1; ++j) {
+            aux = me_formatted_str("%4.2f,  ", matrix[i][j]);
+            aux2 = str_matrix;
+            str_matrix = me_concat_str(str_matrix, aux);
+            me_free_memory((void *)&aux2);
+            me_free_memory((void *)&aux);
+        }
+        aux = me_formatted_str("%4.2f  ],\n\n",  matrix[i][tam_matrix-1]);
+        aux2 = str_matrix;
+        str_matrix= me_concat_str(str_matrix, aux);
+        me_free_memory((void *)&aux2);
+        me_free_memory((void *)&aux);
+    }
+    aux2 = str_matrix;
+    str_matrix= me_concat_str(str_matrix, "]");
+    me_free_memory((void *)&aux2);
+    return str_matrix;
+}
+
--- graphs/matrix_operations.h
+++ graphs/matrix_operations.h
@@ -6,6 +6,7 @@
 #define TRABALHO_PRATICO_GRAFOS_MATRIX_OPERATIONS_H
 #include <stdlib.h>
 
+char *mop_str_matrix_int(double **matrix, int tam_matrix);
 void *mop_create_matrix(size_t tam_type, int tam);
 void *mop_destroy_matrix(void **matrix, int tam);
 #endif //TRABALHO_PRATICO_GRAFOS_MATRIX_OPERATIONS_H
