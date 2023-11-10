ce1df865764d0d2b19b48a1ff410cb77527367a7
Vitorrodrys <Vitorrodrys@gmail.com>
2023-11-10 09:59:14 -0300

    implement conc of strings and function that can formatted strings and return it

--- memory/memory.c
+++ memory/memory.c
@@ -39,4 +39,39 @@ void me_free_several_objects(int quantity, ...){
     ptr = NULL;
     va_end(p);
 }
+char *me_int_to_str(int number){
+    return me_formatted_str("%d\n", number);
+}
+
+char *me_concat_str(char *str, char *other){
+
+    char *new_str = me_memory_alloc(NULL, sizeof(char)* strlen(str) + sizeof (char)* strlen(other) + sizeof (char));
+    strcpy(new_str, str);
+    strcat(new_str, other);
+    return new_str;
+}
+
+int _count_quantity_token(char *str, char tk){
+
+    int quantity = 0;
+    while (strchr(str, tk)){
+        str = strchr(str, tk)+1;
+        quantity++;
+    }
+    return quantity;
+}
+char *me_formatted_str(char *control_str, ...){
+
+    int quantity_paramaters =_count_quantity_token(control_str, '%');
+    va_list p;
+    va_start(p, quantity_paramaters);
+    va_list p_cp;
+    va_copy(p_cp, p);
+    int size = vsnprintf(NULL, 0, control_str, p);
+    char aux[size];
+    vsnprintf(aux, size, control_str, p_cp);
+    va_end(p);
+    va_end(p_cp);
+    return strdup(aux);
+}
 
--- memory/memory.h
+++ memory/memory.h
@@ -5,9 +5,11 @@
 #ifndef UNTITLED70_MEMORY_H
 #define UNTITLED70_MEMORY_H
 #include <stdlib.h>
-
+#include <string.h>
 void me_free_several_objects(int quantity, ...);
 void me_free_memory(void **ptr);
 void *me_memory_alloc(void *ptr, size_t tam);
-
+char *me_int_to_str(int number);
+char *me_concat_str(char *str, char *other);
+char *me_formatted_str(char *control_str, ...);
 #endif //UNTITLED70_MEMORY_H
