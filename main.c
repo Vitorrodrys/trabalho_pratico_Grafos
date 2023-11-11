22c7ed95af0ef17a4a37754f08e113879fe67322
Vitorrodrys <Vitorrodrys@gmail.com>
2023-11-11 19:19:00 -0300

    tests in main function

--- main.c
+++ main.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
-#include "regions/regions.h"
+#include "graphs/regions/regions.h"
 #include "graphs/digraph_routes/digraph_routes.h"
+#include "graphs/flights_graph/flights_graph.h"
 int main() {
+
 
     CurrentFile *f = create_parser("../MalhaAereaUSA.txt");
     Regions *re = create_regions(f);
     Alias *alias_ae = create_alias();
     DigraphRoutes *graph = create_graph_routes(alias_ae,  re, f);
+    FlightsGraph *fligth = create_flitghs_graph(alias_ae, re, f);
     printf(dgrt_str(graph));
-
+    printf("%s", re_str(re));
     graph = destroy_dgrt(graph);
+    destroy_regions(re);
     return 0;
 }
